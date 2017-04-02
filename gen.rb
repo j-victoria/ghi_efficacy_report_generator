c008c24544c4203283cbb454c51e06b779001eff
j_victoria <jvictor5@binghamton.edu>
2017-04-02 16:34:44 +0000

    updating some things

--- gen.rb
+++ gen.rb
@@ -29,39 +29,26 @@ def list_gen (drug_name, disease_name)
     cited_articles = []
     terms = ["efficacy", "clincal trial"]
     terms.each do |name|
-        query = to_uri_able(drug_name) +  $and_uri  +"\"" + disease_name + "\"" + $mesh + $and_uri  + name + $fields + $and_uri #+ URI::encode(name) + $fields
+        query = to_uri_able(drug_name) +  $and_uri  +"\"" + disease_name + "\"" + $mesh + $and_uri  + name + $fields + $and_uri
         pmc_doc = Nokogiri::XML(open(URI::encode(base_etools + "esearch.fcgi?db=" + pmc + "&term=" + query +"&retmax=50")))
         sleep(0.34)
         pm_doc = Nokogiri::XML(open(URI::encode(base_etools + "esearch.fcgi?db=" + pm + "&term=" + query +"&retmax=50")))
-        #print doc
         pmc_ids = pmc_ids + pmc_doc.xpath("//Id").to_s.split(/<\/?Id>/).uniq
         pmc_ids.delete("")
         pm_ids += pm_doc.xpath("//Id").to_s.split(/<[^>]+>/).uniq
         
         sleep(0.34)
     end
-    #print doc
-    #print pmc_ids.count
     pmc_ids.uniq!
-    count = 1
-    #print name
-    #print pmc_ids
     pmc_ids.each do |i|
         sleep(0.34)
-        count += 1
         url = base_oa + "?verb=GetRecord&identifier=oai:pubmedcentral.nih.gov:" + i +"&metadataPrefix=" + pmc
         art_doc =  Nokogiri::XML(open(url)).remove_namespaces!
-        title = art_doc.xpath("//article-title").to_s.split(/<[^>]+>/) * " "
+        
+        title = art_doc.xpath("//title-group").to_s.split(/<[^>]+>/) * " "
         abstract = art_doc.xpath("//abstract").to_s.split(/<[^>]+>/) * " "
         authors = art_doc.xpath("//contrib", "contrib-type" => "author").to_s.split(/<[^>]+>/) * " "
         articles << [title, authors, abstract] unless (title == "")
-        citations = art_doc.xpath("//ref-list").xpath("//ref").xpath("//article-title").to_a
-        citations.each{ |citation| citation =
-        [citation.xpath("//article-title").to_s.split(/<[^>]+>/) * " ", 
-        citation.xpath("//source").to_s.split(/<[^>]+>/) * " ", 
-        ]}#citation.xpath("//pub-id").to_a.each{ |id| id.to_s.split(/<[^>]+>/) * " "}] }
-        #print citations
-        cited_articles += citations
     end
     
     pm_ids.uniq!
@@ -72,20 +59,11 @@ def list_gen (drug_name, disease_name)
     url = base_etools + query
     art_doc =  Nokogiri::XML(open(url)).remove_namespaces!
     
-    #print art_doc
+
     CSV.open(disease_name+ drug_name+".csv", "wb") do |csv|
       articles.each { |art| csv << art }
     end
-    cited_articles.uniq!
-    c_a_s = []
-    cited_articles.each{ |cite| c_a_s << [cite.to_s.split(/<[^>]+>/) * " "] }
-    #cited_articles.each{ |art| art.to_a.each{|title| title.to_s} }
-    #print cited_articles[1]
-    #print cited_articles.count
-    #print c_a_s[1]
-    CSV.open(disease_name+drug_name+"_citations.csv", "wb") do |csv|
-      c_a_s.each {|art| csv << art }
-    end
+
 end
 
 list_gen("Chloroquine+Primaquine", "Plasmodium Vivax")
